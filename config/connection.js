4c1f89007e4b7bf8a7fc9aa3c7722ff2f2225545
Judy Naegeli <jnaegeli@gmail.com>
2020-09-24 16:26:20 -0700

    connection tweaks

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-var connection;
+const connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -13,12 +13,6 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
